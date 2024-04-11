9891434c3b993c4b1b2f66bcc30f6d8011b5c6e1
shubhamsabal_93 <shubhamsabale4733@gmail.com>
2024-04-11 13:29:39 +0530

    recreate

---
+++ src/views/Home/Home.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Navbar from '../../components/Navbar/Navbar'
+
+import './Home.css'
+
+
+function App() {
+  
+  return (
+    
+
+    <div>
+      <Navbar/>
+      
+      <h1 className='heading'>Home Page🏠</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ src/views/News/News.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../../components/Navbar/Navbar'
+
+
+function News() {
+  return (
+    <div>
+      <Navbar/>
+      
+      <h1>News</h1>
+    </div>
+  )
+}
+
+export default News
